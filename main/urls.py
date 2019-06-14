0bd53f25435a81668485fdfde17bbed515a5754c
Larry Shi <shilerong@gmail.com>
2019-06-13 20:48:44 -0400

    added url and win ratio for team list

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('players/', views.player_list, name='player_list'),
     path('players/<int:player_id>/', views.players, name='players'),
+    path('teams/', views.team_list, name='team_list'),
     path('teams/<int:team_id>/', views.teams, name='teams')
 ]
