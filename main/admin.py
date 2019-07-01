3d0aaed46141c4feaba149aa1d495909cb0ab63e
Larry Shi <shilerong@gmail.com>
2019-06-30 22:04:47 -0400

    updated admin control

--- main/admin.py
+++ main/admin.py
@@ -5,8 +5,11 @@
 """
 from django.contrib import admin
 
-from .models import Player, Team
+from .models import Player, Team, TeamGameLog, PlayerGameLog, Game
 
 # Register models
 admin.site.register(Player)
 admin.site.register(Team)
+admin.site.register(TeamGameLog)
+admin.site.register(PlayerGameLog)
+admin.site.register(Game)
