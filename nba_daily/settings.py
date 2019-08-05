bb95ec045a8267acf14c3b10b779fcb7003ead2d
Larry Shi <shilerong@gmail.com>
2019-08-04 22:38:00 -0400

    fixed deployment setting

--- nba_daily/settings.py
+++ nba_daily/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,5 +132,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# if not DEBUG:
-#     django_heroku.settings(locals())
+if not DEBUG:
+    django_heroku.settings(locals())
