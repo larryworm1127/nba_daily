0fa7c9f713fc7602a21cce4421f4728c56548659
Shi, Larry <Larry.shi@Merchantlink.com>
2019-08-09 12:54:16 -0400

    added react stuff for testing

--- nba_daily/settings.py
+++ nba_daily/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
